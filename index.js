72edb1fab2bd25ee07108014cb5a6dae0c9a1099
EdonGashi <edongashi@hotmail.co.uk>
2018-02-25 21:20:06 +0100

    Try initial port but get a new one if it's taken

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 module.exports = {
 
   before: config => {
+    const getPort = require('get-port')
     const { Action, Form, listen, clearHandlers } = require('sharp-pad-forms')
     let port = 5256
     if (config && config['sharp-pad-forms'] && config['sharp-pad-forms'].port) {
       port = config['sharp-pad-forms'].port
     }
 
-    global.clearHandlers = clearHandlers
-    global.Action = Action
-    global.Form = Form
-    listen(port)
+    getPort({ port })
+      .then(newPort => {
+        global.clearHandlers = clearHandlers
+        global.Action = Action
+        global.Form = Form
+        listen(newPort)
+      })
+      .catch(err => {
+        console.error(err)
+      })
   },
 
   beforeEach: config => {
