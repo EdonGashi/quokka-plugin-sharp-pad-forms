54fd3ee3c9d1bb96c1f6370ad36981c8e6e61f9f
EdonGashi <edongashi@hotmail.co.uk>
2018-02-26 04:59:55 +0100

    Removed listener due to issues

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 module.exports = {
 
   before: config => {
-    const getPort = require('get-port')
     const { Action, Form, listen, clearHandlers } = require('sharp-pad-forms')
-    let port = 5256
-    if (config && config['sharp-pad-forms'] && config['sharp-pad-forms'].port) {
-      port = config['sharp-pad-forms'].port
-    }
-
-    global.clearHandlers = clearHandlers
+    global._clearHandlers = clearHandlers
     global.Action = Action
     global.Form = Form
-    getPort({ port })
-      .then(newPort => {
-        listen(newPort)
-      })
-      .catch(err => {
-        console.error(err)
-      })
+    global._listen = listen
   },
 
   beforeEach: config => {
-    global.clearHandlers()
+    global._clearHandlers()
   }
 
 }
